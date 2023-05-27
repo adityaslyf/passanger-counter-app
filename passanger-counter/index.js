50f3b0bdd31ab67b85c794f7588583aa35168572
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-05-27 14:24:23 -0400

    first js project

---
+++ passanger-counter/index.js
@@ -0,0 +1,37 @@
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+  count +=1;
+  countEl.innerText = count
+}
+
+// let SaveBtn = document.getElementById("save-btn");
+// function save (){
+//   count = count +1;
+// SaveBtn.innerText =count;
+
+// }
+
+// function save (){
+//   count +=1;
+//   let Entries = "Previous Entries:";
+//   let firstEntry = document.getElementById("save-btn")
+//   firstEntry.innerText = Entries + count;
+//   console.log(firstEntry)
+
+
+// }
+
+function save (){
+
+let SaveEl = document.getElementById("entries");
+let special = " " + count + " -\ ";
+SaveEl.innerText += special;
+console.log(count)
+
+
+}
+
+
