10a5f47361367ceb993d57ce60d85d43227e65af
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-06-20 00:29:15 +0530

    adding buttons

---
+++ Blackjack-game/index.js
@@ -0,0 +1,53 @@
+// let firstCard  = Math.floor((Math.random() * 100) + 1); 
+// let secondCard = Math.floor((Math.random() * 100) + 1); 
+let firstCard =5
+let secondCard =11
+  let sum = firstCard + secondCard;
+  let hasBlackjack= false;
+  let isAlive = true;
+let message = ""
+
+  
+ 
+// console.log(hasBlackjack)
+// console.log(isAlive)
+ 
+let  messageEl = document.getElementById("message-el");
+
+let sumEl = document.getElementById("sum-el");
+// let sumEl = document.querySelector("#sum-el");
+
+let cardsEl = document.getElementById("cards-el");
+function startGame(){
+
+sumEl.textContent = "Sum: " + sum;
+cardsEl.textContent = "card: " + firstCard + " " + secondCard;
+  if (sum <= 20){
+    message="Do you want to draw a new card? 🙂"
+  }else if(sum ===21) {
+
+    message ="Wohoo! You've got Blackjack! 🥳"
+    hasBlackjack = true;
+  }
+  else{
+message ="You're out of the game! 😭 "
+   isAlive = false
+  }
+  
+(messageEl.textContent) = message;
+ 
+}
+
+function newCard(){
+  console.log( "Drawing a new card from the deck!")
+  let card = 6;
+  sum+= card;
+  startGame();
+}
+
+
+
+
+
+
+
