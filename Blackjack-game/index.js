01f9c328c121337e6d334980ccc8fbc0b3e8d774
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-06-22 22:37:46 +0530

    random number

--- Blackjack-game/index.js
+++ Blackjack-game/index.js
@@ -1,13 +1,18 @@
 // let firstCard  = Math.floor((Math.random() * 100) + 1); 
 // let secondCard = Math.floor((Math.random() * 100) + 1); 
 let firstCard =5
-let secondCard =11
-  let sum = firstCard + secondCard;
+let secondCard =getRandom
+  
   let cards = [firstCard,secondCard]
+  let sum = firstCard + secondCard;
   let hasBlackjack= false;
   let isAlive = true;
 let message = ""
 
+
+function getRandom(){
+return 5;
+}
   
  
 // console.log(hasBlackjack)
@@ -25,9 +30,17 @@ renderGame()
 }
 
 function renderGame(){
+  cardsEl.textContent ="card: "
 
-sumEl.textContent = "Sum: " + sum;
-cardsEl.textContent = "card: " + cards[0] + " " + cards[1];
+//  cardsEl.textContent = "card: " + cards[0] + " " + cards[1];
+
+for (let j =0; j <cards.length; j++){
+ cardsEl.textContent +=cards[j] + " "
+}
+
+sumEl.textContent = "Sum: " + sum
+
+
   if (sum <= 20){
     message="Do you want to draw a new card? 🙂"
   }else if(sum ===21) {
@@ -45,7 +58,7 @@ message ="You're out of the game! 😭 "
 }
 
 function newCard(){
-  console.log( "Drawing a new card from the deck!")
+ 
   let card = 6;
   sum+= card;
 cards.push(card)
@@ -53,6 +66,8 @@ console.log(cards)
   renderGame();
   
 }
+
+
 
 
 
